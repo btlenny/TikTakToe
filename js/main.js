2bb1b99e77dce3b4a4fd4734a9694d2e4250463d
btlenny <btlenny@gmail.com>
2023-09-24 17:07:03 -0700

    adding in win and tie messages

--- js/main.js
+++ js/main.js
@@ -16,22 +16,49 @@ for (let i = 0; i < cells.length; i++) {
     });
   }
  
- 
+
 //Alternate between X and O 
 function handleCellClick(cell) {
   const cellIndex = cell.id;
-  
-  
+
   if (board[cellIndex] === '' && gameActive) {
-   
     cell.textContent = currentPlayer;
     board[cellIndex] = currentPlayer;
-    
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+    const winner = checkWin();
+    if (winner) {
+      gameActive = false;
+      if (winner === 'tie') {
+        alert("It's a tie!");
+      } else {
+        alert(`Player ${winner} wins!`);
+      }
+    } else {
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
   }
-} 
+}
+// Check its a win or a tie
+function checkWin() {
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]             // Diagonals
+  ];
 
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
 
+  if (!board.includes('')) {
+    return 'tie'; // It's a tie if the board is full with no winner
+  }
+
+  return null; // No winner yet
+}
 
 //Reset Button
   resetButton.addEventListener('click', () => {
